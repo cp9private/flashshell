4fca51311b780f84a0a51ac38dd17e023f4ca88b
cp9private <68660795+cp9private@users.noreply.github.com>
2020-07-22 20:15:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+html>
+	<head>
+		<title>Hacked by Flash</title>
+		<iframe width="0" height="0" src="https://www.youtuberepeater.com/watch?v=VstUiw7Df8E#gsc.tab=0" frameborder="0" allowfullscreen></iframe>
+		<link href="https://fonts.googleapis.com/css2?family=Iceland&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+	</head>
+	
+	<body style="background-color:black">
+		<p>-</p>
+		<p>-</p>
+		<div style="color:red;font-size:25px;font-family:Iceland">
+		<center>
+		<img style="width:530px" src="https://image.noelshack.com/fichiers/2020/25/1/1592241199-deface.gif">
+		<h1>Hacked By Flash</h1>
+		</center>
+		</div>
+		<div style="color:white;font-size:25px;font-family:Indie Flower">
+		<center>Almost before we knew it,</center>
+		<center>we had left the ground.</center>
+		<br>
+		<center>Votre Sécurité Est Minable...</center>
+		</div>
+		<div style="color:red;font-size:30px;font-family:Orbitron">
+		<marquee style="color:red;font-size:20px;font-weight:bold;font-family:Iceland">Greatz : 212. | Wazobinks
+		<div style="background-color:grey;width:10.5%;height:3px">
+		</div>
+		</marquee>
+		</div>
+	</body>
+</html>
+
+
+<?php
+
+?>
